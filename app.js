04e637449b8faa53950864ef42e36cab9387dcbf
alceoone <fajar.dm2k@gmail.com.com>
2022-06-05 16:20:54 +0700

    Signed-off-by: alceoone <fajar.dm2k@gmail.com.com> Clear CRUD Product Signed-off-by: alceoone <fajar.dm2k@gmail.com.com>

--- app.js
+++ app.js
@@ -3,74 +3,55 @@ const bodyParser = require('body-parser');
 const connDB = require('./config/database');
 const app = express();
 const PORT = process.env.PORT || 3000;
-// set body parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res)=> {
     return res.send("api alceoone");
 });
 
+// Start Api Transaksi
 // create data / insert data
 app.post('/api/product', (req, res) => {
-    // buat variabel penampung data dan query sql
-    const data = { ...req.body };
-    const querySql = 'INSERT INTO tbl_product SET ?';
 
-    // jalankan query
+    const data = { ...req.body };
+    const querySql = 'INSERT INTO tbl_product SET ?  ';
     connDB.query(querySql, data, (err, rows, field) => {
-        // error handling
         if (err) {
             return res.status(500).json({ message: 'Gagal insert data!', error: err });
         }
-
-        // jika request berhasil
         res.status(201).json({ success: true, message: 'Berhasil insert data!' });
     });
 });
 
 // read data / get data
 app.get('/api/product', (req, res) => {
-    // buat query sql
     const querySql = 'SELECT * FROM tbl_product';
 
-    // jalankan query
     connDB.query(querySql, (err, rows, field) => {
-        // error handling
         if (err) {
             return res.status(500).json({ message: 'Ada kesalahan', error: err });
         }
-
-        // jika request berhasil
         res.status(200).json({ success: true, data: rows });
     });
 });
 
 // update data
-app.put('/api/bootcamp/:id', (req, res) => {
-    // buat variabel penampung data dan query sql
+app.put('/api/product/:id', (req, res) => {
     const data = { ...req.body };
     const querySearch = 'SELECT * FROM tbl_product WHERE id = ?';
     const queryUpdate = 'UPDATE tbl_product SET ? WHERE id = ?';
 
-    // jalankan query untuk melakukan pencarian data
     connDB.query(querySearch, req.params.id, (err, rows, field) => {
-        // error handling
         if (err) {
             return res.status(500).json({ message: 'Ada kesalahan', error: err });
         }
-
-        // jika id yang dimasukkan sesuai dengan data yang ada di db
         if (rows.length) {
-            // jalankan query update
             connDB.query(queryUpdate, [data, req.params.id], (err, rows, field) => {
-                // error handling
                 if (err) {
                     return res.status(500).json({ message: 'Ada kesalahan', error: err });
                 }
-
-                // jika update berhasil
                 res.status(200).json({ success: true, message: 'Berhasil update data!' });
             });
         } else {
@@ -79,29 +60,21 @@ app.put('/api/bootcamp/:id', (req, res) => {
     });
 });
 
-// delete data
-app.delete('/api/bootcamp/:id', (req, res) => {
-    // buat query sql untuk mencari data dan hapus
+// Dellet Data
+app.delete('/api/product/:id', (req, res) => {
     const querySearch = 'SELECT * FROM tbl_product WHERE id = ?';
     const queryDelete = 'DELETE FROM tbl_product WHERE id = ?';
 
-    // jalankan query untuk melakukan pencarian data
     connDB.query(querySearch, req.params.id, (err, rows, field) => {
-        // error handling
         if (err) {
             return res.status(500).json({ message: 'Ada kesalahan', error: err });
         }
 
-        // jika id yang dimasukkan sesuai dengan data yang ada di db
         if (rows.length) {
-            // jalankan query delete
             connDB.query(queryDelete, req.params.id, (err, rows, field) => {
-                // error handling
                 if (err) {
                     return res.status(500).json({ message: 'Ada kesalahan', error: err });
                 }
-
-                // jika delete berhasil
                 res.status(200).json({ success: true, message: 'Berhasil hapus data!' });
             });
         } else {
@@ -109,7 +82,34 @@ app.delete('/api/bootcamp/:id', (req, res) => {
         }
     });
 });
+// End API Product
+
+// Start Api Transaksi
+// create data / insert data
+app.post('/api/transaksi', (req, res) => {
+
+    const data = { ...req.body };
+    const querySql = 'INSERT INTO tbl_transaksi SET ?  ';
+    connDB.query(querySql, data, (err, rows, field) => {
+        if (err) {
+            return res.status(500).json({ message: 'Gagal insert data!', error: err });
+        }
+        res.status(201).json({ success: true, message: 'Berhasil insert data!' });
+    });
+});
+// read data / get data
+app.get('/api/transaksi', (req, res) => {
+    const querySql = 'SELECT * FROM tbl_transaksi';
+
+    connDB.query(querySql, (err, rows, field) => {
+        if (err) {
+            return res.status(500).json({ message: 'Ada kesalahan', error: err });
+        }
+        res.status(200).json({ success: true, data: rows });
+    });
+});
+
+// End API Transaksi
 
 
-// buat server nya
 app.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
---
+++ config/kasir_alceoone.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 05 Jun 2022 pada 11.13
+-- Versi server: 10.4.20-MariaDB
+-- Versi PHP: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kasir_alceoone`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_product`
+--
+
+CREATE TABLE `tbl_product` (
+  `id_product` int(11) NOT NULL,
+  `nama_product` varchar(255) DEFAULT NULL,
+  `jumlah` int(100) DEFAULT NULL,
+  `harga` int(100) DEFAULT NULL,
+  `images` text DEFAULT NULL,
+  `create_at` timestamp NULL DEFAULT NULL,
+  `update_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tbl_product`
+--
+
+INSERT INTO `tbl_product` (`id_product`, `nama_product`, `jumlah`, `harga`, `images`, `create_at`, `update_at`) VALUES
+(1, 'Tahu', 100, 500, 'https://asset.kompas.com/crops/R2_QECkk_qU7-rCN1atAtVxYQz0=/0x0:1000x667/750x500/data/photo/2021/06/10/60c18f4dea381.jpg', '2022-06-05 08:47:14', '2022-06-05 08:47:14'),
+(2, 'tempe', 1000, 500, NULL, NULL, NULL),
+(3, 'Tahu', 100, 500, 'https://asset.kompas.com/crops/R2_QECkk_qU7-rCN1atAtVxYQz0=/0x0:1000x667/750x500/data/photo/2021/06/10/60c18f4dea381.jpg', '2022-06-05 01:47:14', '2022-06-05 01:47:14'),
+(4, 'Tahu', 100, 500, 'https://asset.kompas.com/crops/R2_QECkk_qU7-rCN1atAtVxYQz0=/0x0:1000x667/750x500/data/photo/2021/06/10/60c18f4dea381.jpg', NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  ADD PRIMARY KEY (`id_product`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
